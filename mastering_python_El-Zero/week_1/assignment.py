821cee774509d2d1124cbc1ce8d5b87fe6e020ce
Nada Omar <na0da2021@gmail.com>
2025-10-13 20:42:58 +0300

    week 1 commit

---
+++ mastering_python_El-Zero/week_1/assignment.py
@@ -0,0 +1,28 @@
+# -------------------------------------------------------------------------------
+# this is the assignment solution for week 1 in El-Zero web school python course
+#              this file is created by Nada on Mon, 13-10-2025
+# -------------------------------------------------------------------------------
+
+name = "Nada"
+age = "23"
+country = "Egypt"
+
+print("Name: " + name + "\n" + "Age: " + age + "\n" + "Country: " + country)
+
+print("\n")  # just to print a new line
+
+print(
+    "Hello, My Name Is "
+    + name
+    + " And Iam "
+    + age
+    + " Years Old and I Live in "
+    + country
+    + "."
+)
+
+print("\n")  # just to print a new line
+
+print(type(name))
+print(type(age))
+print(type(country))
---
+++ mastering_python_El-Zero/week_1/first.py
@@ -0,0 +1,12 @@
+print("I Love Python")  # this is a single line comment
+# print("I Love Programming")
+
+print("\x4e\x61\x64\x61")  # this will results in (Nada)
+
+"""
+this is not a
+multiple line string.
+this is string (DOCSTRING) used as 
+clarifation 
+in functions
+"""
